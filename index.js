ec3978a0362ee84f00e43b344e069ac4d19c18d4
Badirkhanov Rovshan Mukhlis <badirkhanov_r@itstep.edu.az>
2024-12-03 22:08:06 +0400

    redefine standart method

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-function sum(a, b) {
-    console.log(arguments);
+const arr = [1, 2, 3, 4];
 
-    return a + b;
+console.log(...arr);
+
+const obj = {
+    *[Symbol.iterator]() {
+        for (let i = 0; i < 3; ++i) {
+            yield i + 50;
+        }
+    }
 }
 
-const sumWithFive = sum.bind(null, 5);
+console.log(...obj);
 
-console.log(sumWithFive(8, 3));
-
-const sumWithFifty = sum.bind(null, 50);
-
-console.log(sumWithFifty(8));
-
-const oneHundredFifty = sum.bind(null, 50, 100);
-
-console.log(oneHundredFifty(1, 2, 3, 4, 5));
+for (const num of obj) {
+    console.log(num);
+}
