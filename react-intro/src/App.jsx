11f21328bd09cd6c9471acf1330a061148546853
Badirkhanov Rovshan Mukhlis <badirkhanov_r@itstep.edu.az>
2024-12-05 21:25:26 +0400

    react fragment

--- react-intro/src/App.jsx
+++ react-intro/src/App.jsx
@@ -6,42 +6,14 @@ function App() {
   const x = 'HELLO';
 
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React {x}
-    //     </a>
-    //   </header>
-    // </div>
-    React.createElement(
-      'div',
-      {
-        className: 'App'
-      },
-      React.createElement(
-        'header',
-        {
-          className: 'App-header'
-        },
-        React.createElement(
-          'img',
-          {
-            src: logo,
-            className: "App-logo",
-            alt: "logo"
-          }
-        )
-      )
-    )
+    <>
+      <div className="App">
+
+      </div>
+      <div className="App">
+
+      </div>
+    </>
   );
 }
 
